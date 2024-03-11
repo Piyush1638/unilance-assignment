cc66a97d5696fdc1c3155de2a776ba648af28b96
Piyush Kumar Singh <21bcs1638@cuchd.in>
2024-03-11 19:36:04 +0530

    Updated README.md

--- src/app/[faq]/page.tsx
+++ src/app/[faq]/page.tsx
@@ -2,13 +2,14 @@
 import { useParams } from 'next/navigation'
 import React from 'react'
 
-const page = () => {
+const Page = () => {  // <-- Updated component name to start with an uppercase letter
     const params = useParams();
-  return (
-    <main className='bg-[#F6F9F4] min-h-screen w-full flex items-center justify-center'>
-        <h3 className='tablet:text-xl text-lg font-semibold'>FAQ's: <span className='text-red-500'>{params.faq}</span></h3>
-    </main>
-  )
+  
+    return (
+        <main className='bg-[#F6F9F4] min-h-screen w-full flex items-center justify-center'>
+            <h3 className='tablet:text-xl text-lg font-semibold'>FAQ&apos;s: <span className='text-red-500'>{params.faq}</span></h3>
+        </main>
+    );
 }
 
-export default page
+export default Page;
